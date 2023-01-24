dd44211401bb4789e6fb38be0c57489b8df1e9c4
Stas Kotelnykov <staskotelnykov@gmail.com>
2023-01-24 02:48:55 +0200

    List changed to Map

--- src/main/java/com/goit/feature/NoteService.java
+++ src/main/java/com/goit/feature/NoteService.java
@@ -3,35 +3,52 @@ package com.goit.feature;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 @RequiredArgsConstructor
 @Service
 public class NoteService {
-    private List<Note> noteList = new ArrayList<>();
+//    private List<Note> noteList = new ArrayList<>();
+    private Map<String, Note> noteList = new HashMap<>();
 
 
-    public List<Note> listAll() {
+//    public List<Note> listAll() {
+//        return noteList;
+//    }
+
+    public Map<String, Note> listAll() {
         return noteList;
     }
 
     public Note add(Note note) {
+//        String tempId = UUID.randomUUID().toString();
+//        while(isMatch(tempId, listAll())) {
+//            tempId = UUID.randomUUID().toString();
+//        }
+//        note.setId(tempId);
+//        return note;
+
         String tempId = UUID.randomUUID().toString();
         while(isMatch(tempId, listAll())) {
             tempId = UUID.randomUUID().toString();
         }
         note.setId(tempId);
+        listAll().put(tempId, note);
         return note;
     }
 
     public Note getById(String id) {
-       return listAll().stream().filter(note -> note.getId().equals(id)).findFirst().orElseThrow(IllegalArgumentException::new);
+//       return listAll().stream().filter(note -> note.getId().equals(id)).findFirst().orElseThrow(IllegalArgumentException::new);
+        return listAll().get(id);
     }
 
     public void deleteById(String id) {
-        listAll().remove(getById(id));
+//        listAll().remove(getById(id));
+        if(!isMatch(id, listAll())) {
+            throw new RuntimeException("Note with " + id + " doesn't exist!");
+        } else {
+            listAll().remove(id);
+        }
     }
 
     public void update(Note note) {
@@ -40,8 +57,12 @@ public class NoteService {
         noteForUpdate.setContent(note.getContent());
     }
 
-    private static boolean isMatch(String tempId, List<Note> noteList) {
-        return noteList.stream()
-                .anyMatch(n -> n.getId().equals(tempId));
+//    private static boolean isMatch(String tempId, List<Note> noteList) {
+//        return noteList.stream()
+//                .anyMatch(n -> n.getId().equals(tempId));
+//    }
+
+    private static boolean isMatch(String tempId, Map<String, Note> noteList) {
+        return noteList.containsKey(tempId);
     }
 }
